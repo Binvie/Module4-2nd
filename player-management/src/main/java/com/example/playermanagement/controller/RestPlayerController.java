1f186719fd5491e0f20fc8e08b9d1231ea872c01
Binvie <thien97.night1@gmail.com>
2023-10-02 16:12:21 +0700

    ok3

--- player-management/src/main/java/com/example/playermanagement/controller/RestPlayerController.java
+++ player-management/src/main/java/com/example/playermanagement/controller/RestPlayerController.java
@@ -6,29 +6,43 @@ import com.example.playermanagement.service.IPlayerService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-import static com.oracle.util.Checksums.update;
-@CrossOrigin("*")
 @RestController
 @RequestMapping("api/players")
+@CrossOrigin("*")
 public class RestPlayerController {
     @Autowired
     private IPlayerService iPlayerService ;
 
-    @GetMapping()
-    public ResponseEntity<Player> showList(){
-        List<Player> playerDtosList = iPlayerService.findAll();
-        if (playerDtosList.isEmpty()){
+    @GetMapping("")
+    public ResponseEntity<Page<Player>> showList(@RequestParam(defaultValue = "0",required = false) int page,
+                                                    @RequestParam(defaultValue = "",required = false)String name){
+        Pageable pageable = PageRequest.of(page,2);
+        Page<Player> playerDtoPage = iPlayerService.findAllPlayer(pageable,name);
+        if (playerDtoPage.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return  new ResponseEntity<>(HttpStatus.OK);
+        return  new ResponseEntity<>(playerDtoPage,HttpStatus.OK);
     }
-    @PostMapping("")
+
+    @PostMapping("/add")
+    public ResponseEntity<?> save(@RequestBody PlayerDto playerDto){
+        if (playerDto == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Player player = new Player();
+        BeanUtils.copyProperties(playerDto,player);
+        iPlayerService.save(player);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("")
     public ResponseEntity<Player> Update(@PathVariable int id, @RequestBody PlayerDto playerDto){
         Player player = iPlayerService.findById(id).get();
         if (player == null){
